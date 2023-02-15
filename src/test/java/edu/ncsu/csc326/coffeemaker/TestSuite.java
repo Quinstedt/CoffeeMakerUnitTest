cdc939cb17685b69fd4f81ce0a1527f1660964bb
Quinstedt <90027419+Quinstedt@users.noreply.github.com>
2023-02-15 22:53:49 +0100

    RecipeTest class test have been updated. CoffeeMakerTest is completed. A TestSuite has been created to execute all test classes.

---
+++ src/test/java/edu/ncsu/csc326/coffeemaker/TestSuite.java
@@ -0,0 +1,9 @@
+package edu.ncsu.csc326.coffeemaker;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ CoffeeMakerTest.class, RecipeTest.class })
+public class TestSuite {
+}
