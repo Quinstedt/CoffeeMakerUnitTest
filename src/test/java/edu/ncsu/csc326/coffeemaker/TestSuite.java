3868fc177d583980c669a92237a82d1c5d3af7f1
Quinstedt <90027419+Quinstedt@users.noreply.github.com>
2023-02-20 11:04:28 +0100

    Deleting invTest

--- src/test/java/edu/ncsu/csc326/coffeemaker/TestSuite.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/TestSuite.java
@@ -4,6 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ CoffeeMakerTest.class, RecipeTest.class, InventoryTest.class, RecipeBookTest.class })
+@Suite.SuiteClasses({
+        CoffeeMakerTest.class,
+        RecipeTest.class,
+        InventoryTest.class,
+        RecipeBookTest.class })
 public class TestSuite {
 }
